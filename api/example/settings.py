b62626b0a83d51e7365d13592718ea818ff1ecc8
Claudio Tiecher <ctiecher@gmail.com>
2024-04-15 15:20:35 +0200

    Moved server app and added to django settings

--- api/example/settings.py
+++ api/example/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.gis',
-    #'server',
+    'rest_framework',
+    'server',
 ]
 
 MIDDLEWARE = [
