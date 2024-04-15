b2d2124ea0f076bf37b605a3ee8dafd45f4f9a81
Claudio Tiecher <ctiecher@gmail.com>
2024-04-15 16:23:33 +0200

    Created raw model and first migration

--- api/server/models.py
+++ api/server/models.py
@@ -1,3 +1,11 @@
-from django.db import models
+from django.contrib.gis.db import models
 
-# Create your models here.
+
+class Municipality(models.Model):
+    name = models.CharField(max_length=50)
+    coordinates = models.MultiPolygonField()
+    crs = models.CharField()
+    collection = models.CharField()
+
+    def __str__(self):
+        return self.name
