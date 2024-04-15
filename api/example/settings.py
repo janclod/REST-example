4fa80a1bbb589ff004193c7c930c16d20ff52b1e
Claudio Tiecher <ctiecher@gmail.com>
2024-04-15 14:17:14 +0200

    Switch to postgis db

--- api/example/settings.py
+++ api/example/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.gis',
+    'server',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,9 @@ WSGI_APPLICATION = 'example.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'test',
+        'USER': 'geodjango',
     }
 }
 
