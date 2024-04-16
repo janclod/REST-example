5930b9bcb38fc785a6ba9b26c9a2eb58ece4da2b
Claudio Tiecher <ctiecher@gmail.com>
2024-04-16 15:46:44 +0200

    Set pagination to 100, reintroduce POST, PUT, DE LETE methods on APIview

--- server/models.py
+++ server/models.py
@@ -5,5 +5,7 @@ class Municipality(models.Model):
     name = models.CharField(max_length=50)
     coordinates = models.MultiPolygonField()
 
+    class Meta:
+        ordering = ['id']
     def __str__(self):
         return self.name
--- server/views.py
+++ server/views.py
@@ -1,17 +1,34 @@
 from server.models import Municipality
 from server.serializers import MunicipalitySerializer
+from rest_framework import mixins
 from rest_framework import generics
 
-class MunicipalityList(generics.ListAPIView):
+class MunicipalityList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
     """
     List all municipalities, or create a new municipality.
     """
     queryset = Municipality.objects.all()
     serializer_class = MunicipalitySerializer
 
-class MunicipalityDetail(generics.RetrieveUpdateDestroyAPIView):
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+class MunicipalityDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
     """
     Retrieve, update or delete a municipality instance.
     """
     queryset = Municipality.objects.all()
     serializer_class = MunicipalitySerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
