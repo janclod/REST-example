6a98e2c10d918516a2dda872a91dfee66f9976c4
Claudio Tiecher <ctiecher@gmail.com>
2024-04-16 11:56:14 +0200

    Add exmaple.json, exclude from git, add allowed hosts

--- example/settings.py
+++ example/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-j_wj9gyjw^zrlat9m#1!m$efau$fem74_b=6e+mt30&6zq5k^x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ '127.0.0.1' ]
 
 
 # Application definition
